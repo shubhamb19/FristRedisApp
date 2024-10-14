6d0df5dc3667c19d101c4eedefa2c3fecdf76d17
shubhamb19 <shubhambhuse19@gmail.com>
2024-10-14 20:39:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+namespace FristRedisApp
+{
+    internal class Program
+    {
+        public static  String redisServer = "localhost";
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer conObject =  ConnectionMultiplexer.Connect(redisServer);
+            IDatabase db = conObject.GetDatabase();
+            db.StringSet("GEnder", "MAle");
+            string value = db.StringGet("GEnder");
+            Console.WriteLine($"Value for 'Gender': {value}");
+        }    
+    }
+}
